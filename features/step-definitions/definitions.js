fc470b55d191394f928d75c928b0320d4d999e8f
renan chagas <renan.chagas.wanderley@lego.com>
2018-04-17 21:55:56 +0200

    Initial commit

---
+++ features/step-definitions/definitions.js
@@ -0,0 +1,8 @@
+// stepdefinitions.js
+const expect = require('chai').expect;
+
+module.exports = function () {
+  this.Given(/^I go to the website "([^"]*)"$/, (url) => {
+    browser.url(url);
+  });
+}
